3e00131f47d47e5419a8ac1dc268f3cbd62cb812
Andrey Melnikov <an.melnikov@rambler-co.ru>
2015-12-29 19:34:25 +0300

    Testing Api interface

--- app/src/test/java/com/andrey7mel/testrx/model/api/ApiInterfaceTest.java
+++ app/src/test/java/com/andrey7mel/testrx/model/api/ApiInterfaceTest.java
@@ -3,13 +3,12 @@ package com.andrey7mel.testrx.model.api;
 import com.andrey7mel.testrx.model.dto.RepositoryDTO;
 import com.andrey7mel.testrx.other.BaseTest;
 import com.squareup.okhttp.HttpUrl;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 import com.squareup.okhttp.mockwebserver.Dispatcher;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
 import com.squareup.okhttp.mockwebserver.RecordedRequest;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,11 +17,12 @@ import java.util.List;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.RxJavaCallAdapterFactory;
-import rx.Observer;
+import rx.observers.TestSubscriber;
 
 public class ApiInterfaceTest extends BaseTest {
 
-
+    private final String TEST_OWNER = "TEST_OWNER";
+    private final String TEST_REPO = "TEST_REPO";
     MockWebServer server;
     ApiInterface apiInterface;
 
@@ -35,32 +35,26 @@ public class ApiInterfaceTest extends BaseTest {
             @Override
             public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
 
-                if (request.getPath().equals("users/test/repos")) {
-                    return new MockResponse().setResponseCode(200).setBody("test");
-                } else if (request.getPath().equals("v1/check/version/")) {
+                if (request.getPath().equals("/users/" + TEST_OWNER + "/repos")) {
                     return new MockResponse().setResponseCode(200)
-                            .setBody("version=9");
-                } else if (request.getPath().equals("/v1/profile/info")) {
-                    return new MockResponse().setResponseCode(200).setBody("{\\\"info\\\":{\\\"name\":\"Lucas Albuquerque\",\"age\":\"21\",\"gender\":\"male\"}}");
+                            .setBody(testUtils.readString("json/repos"));
+                } else if (request.getPath().equals("/repos/" + TEST_OWNER + "/" + TEST_REPO + "/branches")) {
+                    return new MockResponse().setResponseCode(200)
+                            .setBody(testUtils.readString("json/branches"));
+                } else if (request.getPath().equals("/repos/" + TEST_OWNER + "/" + TEST_REPO + "/contributors")) {
+                    return new MockResponse().setResponseCode(200)
+                            .setBody(testUtils.readString("json/contributors"));
                 }
                 return new MockResponse().setResponseCode(404);
             }
         };
         server.setDispatcher(dispatcher);
-        HttpUrl baseUrl = server.url("/v1/chat/");
+        HttpUrl baseUrl = server.url("/");
 
         Retrofit.Builder builder = new Retrofit.Builder().
                 baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
-
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        OkHttpClient httpClient = new OkHttpClient();
-        httpClient.interceptors().add(interceptor);
-
-        builder.client(httpClient);
 
         apiInterface = builder.build().create(ApiInterface.class);
     }
@@ -68,39 +62,43 @@ public class ApiInterfaceTest extends BaseTest {
 
     @Test
     public void testGetRepositories() throws Exception {
-        System.out.print("AAA");
-//        apiInterface.getRepositories("a").toBlocking().forEach(new Action1<List<RepositoryDTO>>() {
-//            @Override
-//            public void call(List<RepositoryDTO> repositoryDTOs) {
-//                System.out.println("BBB");
-//
-//            }
-//        });
 
-        apiInterface.getRepositories("test").subscribe(new Observer<List<RepositoryDTO>>() {
-            @Override
-            public void onCompleted() {
-                testUtils.log("onCompleted");
-            }
+        TestSubscriber<List<RepositoryDTO>> testSubscriber = new TestSubscriber<>();
+        apiInterface.getRepositories(TEST_OWNER).subscribe(testSubscriber);
 
-            @Override
-            public void onError(Throwable e) {
-                testUtils.log("onError");
-            }
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertValueCount(1);
 
-            @Override
-            public void onNext(List<RepositoryDTO> list) {
-                testUtils.log("onNext");
+        List<RepositoryDTO> actual = testSubscriber.getOnNextEvents().get(0);
 
-            }
-        });
-        ;
+        assertEquals(actual.size(), 7);
+        assertEquals(actual.get(0).getName(), "Android-Rate");
+        assertEquals(actual.get(0).getFullName(), "andrey7mel/Android-Rate");
+        assertEquals(actual.get(0).getId(), 26314692);
     }
 
+    @Test
+    public void testGetRepositoriesIncorrect() throws Exception {
+        try {
+            apiInterface.getRepositories("IncorrectRequest").subscribe();
+            fail();
+        } catch (Exception expected) {
+            assertEquals("HTTP 404 OK", expected.getMessage());
+        }
+    }
+
+
+
     public void testGetContributors() {
+
     }
 
     public void testGetBranches() {
 
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.shutdown();
     }
 }
